c7fdef52a863304dc00cf8108853ccc2acf1dde9
CodingAbdullah <abdullahmd326@gmail.com>
2024-01-28 07:07:57 -0500

    Adding custom backend Punctuation Queue datatype along with filtering Media/Style tag functions

---
+++ backend/utilFunctions/filterStyleTags.ts
@@ -0,0 +1,12 @@
+// These are the markups supported by Medium.com
+// Filtering out style related elements <em><a><code><strong><blockquote><pre><mark><img>
+export default function filterStyleTags(htmlString: string): string {        
+    return htmlString.replace(/<em\b[^>]*>(.*?)<\/em>/g, "$1")
+        .replace(/<a\b[^>]*>(.*?)<\/a>/g, "$1")
+        .replace(/<code\b[^>]*>(.*?)<\/code>/g, "$1")
+        .replace(/<strong\b[^>]*>(.*?)<\/strong>/g, "$1")
+        .replace(/<blockquote\b[^>]*>(.*?)<\/blockquote>/g, "$1")
+        .replace(/<pre\b[^>]*>(.*?)<\/pre>/g, "$1")
+        .replace(/<mark\b[^>]*>(.*?)<\/mark>/g, "$1")
+        .replace(/<img\b[^>]*>/g, "");
+}
