4de8857d43a22a23c824345c329b0cd9d9da5ddb
CodingAbdullah <abdullahmd326@gmail.com>
2024-01-29 21:00:48 -0500

    Refactoring backend

--- backend/utilFunctions/uploadTTSFile.ts
+++ backend/utilFunctions/uploadTTSFile.ts
@@ -4,37 +4,37 @@ import { v4 } from "uuid";
 import VoiceDataType from '../dataTypes/VoiceDataType';
 import * as dotenv from 'dotenv';
 
-dotenv.config(); // Configure environment variables
-
-// After having filtered all the text from HTML document, generate an audio file
-// Initiating OpenAI client and setting S3 Bucket configurations
-let client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-let S3ClientConfiguration: S3ClientConfig  = {
-    region: process.env.AWS_REGION,
-    credentials: {
-       secretAccessKey: process.env.AWS_SECRET_KEY!,
-       accessKeyId: process.env.AWS_ACCESS_ID!,
-    }
-};
-
-// Initiate S3 Bucket
-let S3Bucket = new S3Client([S3ClientConfiguration]);
+// Configure environment variables
+dotenv.config();
 
 // Generate audio stream from text and then upload it as an .mp3 file to AWS S3
 export async function uploadTTSFile(documentText: string, voiceType: VoiceDataType): Promise<any[]> {    
     try {
-        let audioFileID = v4().split("-")[0]; // Generate random ID
-        let conversion = await client.audio.speech.create({ 
-            model: 'tts-1', 
-            voice: voiceType.voice, 
-            input: documentText 
-        });
+        // Generate random ID
+        let audioFileID = v4().split("-")[0];
+
+        // Initiating S3 client by setting S3 Bucket configurations
+        let S3ClientConfiguration: S3ClientConfig  = {
+            region: process.env.AWS_REGION,
+            credentials: {
+                secretAccessKey: process.env.AWS_SECRET_KEY!,
+                accessKeyId: process.env.AWS_ACCESS_ID!,
+            }
+        };
+
+        // After having filtered all the text from HTML document, generate an audio file
+        let conversion = 
+            await new OpenAI({ apiKey: process.env.OPENAI_API_KEY }).audio.speech.create({ 
+                model: 'tts-1', 
+                voice: voiceType.voice, 
+                input: documentText 
+            });
 
         // Upon successful request, create audio stream using Buffer
         let audioStream = Buffer.from(await conversion.arrayBuffer());
 
         // Now upload audio data stream as an audio file object to S3
-        await S3Bucket.send(new PutObjectCommand({
+        await new S3Client([S3ClientConfiguration]).send(new PutObjectCommand({
             Bucket: process.env.AWS_S3_BUCKET_NAME,
             Key: 'Medium Article-' + audioFileID + '.mp3',
             Body: audioStream
--- backend/utilFunctions/uploadTextFile.ts
+++ backend/utilFunctions/uploadTextFile.ts
@@ -4,27 +4,26 @@ import * as dotenv from 'dotenv';
 
 dotenv.config(); // Configure environment variables
 
-// Setting configuration to be used by S3 Bucket
-const S3BucketConfiguration: S3ClientConfig = {
-    region: process.env.AWS_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_ID!,
-      secretAccessKey: process.env.AWS_SECRET_KEY!
-    }
-  };
-
-// Initiating S3 Bucket
-let S3Bucket = new S3Client([S3BucketConfiguration]);
-
 // After having filtered all text from HTML document, generate a text file
 export async function uploadTextFile(documentText: string): Promise<any[]> {
-    
+    // Setting configuration to be used by S3 Bucket
+    const S3BucketConfiguration: S3ClientConfig = {
+        region: process.env.AWS_REGION,
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_ID!,
+            secretAccessKey: process.env.AWS_SECRET_KEY!
+        }
+    };
+
     // Assign Bucket name where file is to be uploaded
     // Assign Key using the help of the UUID library
     // Assign Document text to be equivalent to the generated text filtered and refined
     try {
+        // Generating a random ID identifier for text file
         let textFileID = v4().split("-")[0];
-        await S3Bucket.send(new PutObjectCommand({
+
+        // Initiating S3 Bucket and sending a PutObject command
+        await new S3Client([S3BucketConfiguration]).send(new PutObjectCommand({
             Bucket: process.env.AWS_S3_BUCKET_NAME,
             Key: 'Medium-Article-' + textFileID + '.txt',
             Body: documentText
