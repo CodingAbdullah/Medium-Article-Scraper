eb90ed18ce15cfb979bcaaf1ed35f1a87ffb66df
CodingAbdullah <abdullahmd326@gmail.com>
2024-11-10 08:09:33 -0500

    ...............

--- src/app/dataTypes/ArticleInsightsType.ts
+++ src/app/dataTypes/ArticleInsightsType.ts
@@ -1,6 +1,8 @@
+import { Entity } from "@aws-sdk/client-comprehend";
+
 // Custom type for creating Medium article insights
 export interface ArticleInsights {
     keyPhrases: string[];
-    entities: AWS.Comprehend.Entity[];
+    entities: Entity[];
     dominantLanguage: string;
 }
